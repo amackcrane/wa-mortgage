4227766c71030b44474c36a2f4cf9cd2ba5597c8
Austen Mack-Crane <austen@austenmanesMBP7.fios-router.home>
2020-01-30 17:40:25 -0500

    basic SVM working

--- svm.R
+++ svm.R
@@ -26,7 +26,7 @@ load(file='prepped.data')
 #' ### Spec model
 #+ spec, echo=TRUE, results='show'
 
-svc <- svm$SVC(kernel='linear')
+svc <- svm$LinearSVC(loss='squared_hinge', dual=FALSE)
 
 X <- loans.fixed
 y <- outcome.fixed
@@ -50,7 +50,7 @@ print(svc$score(testX, testy))
 #+ cv, echo=TRUE, results='show', eval=TRUE
 
 pg <- list()
-pg$C <- c(.01, .1, 1, 10)
+pg$C <- c(.0001, .001, .01, .1, 1)
 
 svcv <- selection$GridSearchCV(estimator=svc, param_grid=pg, scoring='accuracy', cv=5L)
 
@@ -67,6 +67,11 @@ svcv <- tryCatch({
 #' ### Viz
 #+ viz, echo=FALSE, results='show'
 
+cv.results <- data.frame(svcv$cv_results_)
+cv.results$param_C <- unlist(cv.results$param_C)
+
+plt <- ggplot(cv.results, aes(param_C, mean_test_score)) + geom_line()
+print(plt)
 
 end <- Sys.time()
 time <- end - start
@@ -74,8 +79,9 @@ print(time)
 
 #' ### Timing
 #' one model, 3000 obs -- 30s  
-#' pg=4, cv=5, full set -- 
-
+#' pg=4, cv=5, full set -- 33m  
+#' pg=5, cv=5, full set -- 29m (C range was lower...)  
+#' pg=4, cv=5, SVC (LIBSVC) -- 10 hrs & still chuggin  
 
 
 
